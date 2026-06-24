2282162ed0538e220e96b6357be1e9cd1c99e978
thxiara <liamclases1234@gmail.com>
2026-06-23 21:30:58 -0300

    agregué anotaciones, mejoré el html y css, cambie colores

--- front/dist/js/catalogo.js
+++ front/dist/js/catalogo.js
@@ -1,14 +1,12 @@
+//constantes sacadas del html
 const botonFiccion = document.getElementById('boton-ficcion');
 const botonNoFiccion = document.getElementById('boton-noficcion');
 const botonInfantil = document.getElementById('boton-infantil');
-
 const cajaFiccion = document.getElementById('generos-ficcion');
 const cajaNoFiccion = document.getElementById('generos-noficcion');
 const cajaInfantil = document.getElementById('generos-infantil');
-
 const botonAplicar = document.getElementById('btn-aplicar');
-
-// Oculta todas las cajas de checkboxes
+//le quita el active y display a todas las categorias
 function quitarFiltros() {
     cajaFiccion.classList.add('d-none');
     cajaNoFiccion.classList.add('d-none');
@@ -18,33 +16,37 @@ function quitarFiltros() {
     botonNoFiccion.classList.remove('active');
     botonInfantil.classList.remove('active');
 }
-
+//quita el checked a todos los generos
 function limpiarCheckboxes() {
     document.querySelectorAll('.check-genero').forEach(cb => cb.checked = false);
 }
-
+//coso para filtrar
 function filtrarCatalogo() {
+    //agarra la categoria active y los generos checkeados y agrega los generos
+    //checkeados a la lista d generosBuscados.
     const categoriaActiva = document.querySelector('.categorias li.active').getAttribute('data-categoria');
     const checkboxesTildados = document.querySelectorAll('.check-genero:checked');
-    
     const generosBuscados = [];
     checkboxesTildados.forEach(cb => generosBuscados.push(cb.value));
-
     const todosLosLibros = document.querySelectorAll('.tarjeta-libro');
-
     todosLosLibros.forEach(libro => {
-        const catLibro = libro.getAttribute('data-categoria');
-        const genLibro = libro.getAttribute('data-genero');
-
-        const coincideCategoria = (catLibro === categoriaActiva);
-        
+        //agarra la categoria y genero de cada libro, se fija si la categoría
+        //coincide con la categoría activa
+        const categoriaLibro = libro.getAttribute('data-categoria');
+        const generoLibro = libro.getAttribute('data-genero');
+        const coincideCategoria = (categoriaLibro === categoriaActiva);
+        //todo esto es para ver si coinciden los generos. Por defecto es false pq pintó
+        //si la lista de generos buscados está vacía, es true pq no se especificó
+        //si no está vacía, se fija si el libro tiene algún genero de los buscados
         let coincideGenero = false;
         if (generosBuscados.length === 0) {
             coincideGenero = true; 
         } else {
-            coincideGenero = generosBuscados.includes(genLibro);
+            coincideGenero = generosBuscados.includes(generoLibro);
         }
-
+        //si para el libro coinciden tanto la categoría como el género, se le quita
+        //el d-none (display:none pero en bootstrap) y así se vuelve visible
+        //sino, se le agrega el d-none
         if (coincideCategoria && coincideGenero) {
             libro.classList.remove('d-none');
         } else {
@@ -52,7 +54,8 @@ function filtrarCatalogo() {
         }
     });
 }
-
+//al botón de cada categoría hace que al hacerse click se limpien los filtros anteriores,
+//se vuelva visible la caja de generos de esa categoría y se filtre el catálogo
 botonFiccion.addEventListener('click', function() {
     quitarFiltros();
     limpiarCheckboxes();
@@ -60,7 +63,6 @@ botonFiccion.addEventListener('click', function() {
     botonFiccion.classList.add('active');
     filtrarCatalogo();
 });
-
 botonNoFiccion.addEventListener('click', function() {
     quitarFiltros();
     limpiarCheckboxes();
@@ -68,7 +70,6 @@ botonNoFiccion.addEventListener('click', function() {
     botonNoFiccion.classList.add('active');
     filtrarCatalogo();
 });
-
 botonInfantil.addEventListener('click', function() {
     quitarFiltros();
     limpiarCheckboxes();
@@ -76,10 +77,10 @@ botonInfantil.addEventListener('click', function() {
     botonInfantil.classList.add('active');
     filtrarCatalogo();
 });
-
+//esto es para confirmar los filtros
 botonAplicar.addEventListener('click', function(e) {
-    e.preventDefault();
     filtrarCatalogo();
 });
-
+//para q se filtre el catalogo con lo predeterminado
+// en html al cargar la página
 filtrarCatalogo();
